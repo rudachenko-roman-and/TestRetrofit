c37df2f493f977fb39018853feaf8e6f909fad6f
Roman <rudachenko93@mail.ru>
2024-07-04 21:57:41 +0500

    Добавлен HttpLoggingInterceptor и OkHttpClient

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,9 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/rudachenkoroman/testretrofit/RetrofitInstance.kt
+++ app/src/main/java/com/rudachenkoroman/testretrofit/RetrofitInstance.kt
@@ -1,15 +1,30 @@
 package com.rudachenkoroman.testretrofit
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object{
         private const val BASE_URL = "https://dummyjson.com"
-        private val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+
+        private val retrofit by lazy {
+
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL).client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        }
+
         val productApi: ProductApi = retrofit.create(ProductApi::class.java)
     }
 }
